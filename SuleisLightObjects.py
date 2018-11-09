79fd15d3fa2e4014ec2d9b42161e0788e732b115
danielweidman <dww60@case.edu>
2018-11-09 17:55:07 -0500

    Added SunPattern and Dynamic, changed color dicts to status dicts

--- SuleisLightObjects.py
+++ SuleisLightObjects.py
@@ -1,18 +1,21 @@
 import json, uuid
 import pandas as pd
 from abc import ABC, abstractmethod
+import datetime
+import pytz
 
-class MainDatabase:
+
+
+class MainDatabase: #Class that represents main database of users, strips, sections, and active patterns
     def __init__(self):
-        self.users_database = pd.DataFrame(columns=["User ID", "User Object"])
-        self.strips_database = pd.DataFrame(columns=["Strip ID", "Strip Object"]) #use to make sure one strip isn't attached to multiple accounts
-
+        self.users_database = pd.DataFrame(columns=["User ID", "User Object"]) #DataFrame of users for fast user locating
+        self.strips_database = pd.DataFrame(columns=["Strip ID", "Strip Object"]) #use to make sure one strip isn't attached to multiple accounts, and fast strip object locating
+        self.DYNAMIC_PATTERN_CODES = {'Color cycle':'cc'}
     def return_user_object(self,user): #returns the user's object if user is returning, or makes a new one and then returns it if it is a new user. If user is none (no logged in?), returns false
         if user != None:
 
-            #user.profile.firstName
-            #user.id
             try:
+                #Existing user
                 user_obj = self.users_database.loc[user.id]["User Object"]
                 return user_obj
             except:
@@ -23,25 +26,28 @@ class MainDatabase:
         else:
             return False
 
-    def check_and_add_strip(self, strip_id, leds_count, display_name): #if strip already exists, returns false, if strip is new, adds it to list and returns True
+    def check_and_add_strip(self, strip_id, leds_count, display_name): #if strip already exists, returns false, if strip is new, adds it to list and return the strip object
         try:
-            strip_obj = self.strips_database.loc[strip_id]["Strip Object"]
-            return False
+            strip_obj = self.strips_database.loc[strip_id]["Strip Object"] #Returns error if strip not already in database
+
+            return False #Returns error if strip not already in database
+
         except:
+            #Add the strip
             new_strip = Strip(strip_id,leds_count,display_name)
             self.strips_database = self.strips_database.append(pd.Series({"Strip ID": strip_id, "Strip Object": new_strip}, name=strip_id))
             return new_strip
 
 
-class User:
-    def __init__(self, user_id, first_name, last_name, main_database):
+class User: #Class to represent a user. Can aggregate multiple strips
+    def __init__(self, user_id, first_name, last_name, main_database): #user_id from the OIDC/Okta service, see app.py
         self.user_id = user_id
         self.first_name = first_name
         self.last_name = last_name
         self.light_strips = {}
         self.main_database = main_database
 
-    def add_strip(self, strip_id, leds_count, display_name):
+    def add_strip(self, strip_id, leds_count, display_name): #add new strip to user with given parameters
 
         if display_name in self.light_strips.keys():
             return "Error: light strip of same name already present in user account"
@@ -58,25 +64,24 @@ class User:
 
 
 
-class Strip:
+class Strip: #Class representing a Strip. Can aggregate multiple sections.
     def __init__(self,strip_id,leds_count,display_name):
         self.leds_count = leds_count
         self.strip_id = strip_id
         self.display_name = display_name
-        self.leds_and_sections = []
-        self.default_section = Section(self,0, leds_count-1, "Section 1")
-        #self.default_section.set_mode({"pattern":"solid_color","info":{"R":0,"G":193,"B":0}}) #Default mode is green (not white for now because sometimes the lights turn white if not connected properly)
-        self.sections_list = [self.default_section]
+        self.leds_and_sections = [] #List that stores a pointer to the relevant section for each LED (could makke this more memory efficient on average by storing only ranges and sections)
+        self.default_section = Section(self,0, leds_count-1, "Section 1") #Make aa default section for this strip
+        self.sections_list = [self.default_section] #List of all active sections in this strip. Inactive sections (those covered fully by new section(s)) will be removed.
         self.update_leds_and_sections()
         self.leds_and_statuses = None
 
-    def get_section_by_id(self, section_id):
+    def get_section_by_id(self, section_id): #Returns the section belonging to this Strip with the given id (or False)
         for section in self.sections_list:
             if section.section_id == section_id:
                 return section
         return False
 
-    def get_pattern_by_id(self, pattern_id):
+    def get_pattern_by_id(self, pattern_id):#Returns the pattern belonging to this Strip with the given id (or False)
         for section in self.sections_list:
             if section.current_mode.pattern_id == pattern_id:
                 print("section.current_mode.pattern_id")
@@ -85,11 +90,11 @@ class Strip:
                 return section.current_mode
         return False
 
-    def add_new_section_and_update(self,start_led,end_led):
+    def add_new_section_and_update(self,start_led,end_led): #Add new section to the strip (and test that the range is valid
         if start_led < 0:
             return "Error: section start led index is below 0"
         if end_led > self.leds_count-1:
-            return "Error: section end led index is greater than that of the last led on the strip {self.leds_count-1}"
+            return f"Error: section end led index is greater than that of the last led on the strip {self.leds_count-1}"
         self.sections_list.append(Section(self,start_led,end_led, f"Section {len(self.sections_list)+1}"))
         self.update_leds_and_sections()
         self.update_light_statuses()
@@ -98,16 +103,15 @@ class Strip:
 
 
     def update_leds_and_sections(self):
-        #CALL THIS ANY TIME A NEW section IS ADDED (it's called automatically by add_new_section_and_update)
-        #section priorities are in reverse-priority: if two overlap, the one added later prevails
-        #sections have their led counts updated here
+        #CALL THIS ANY TIME A NEW Section IS ADDED (it's called automatically by add_new_section_and_update)
+        #Section priorities are in reverse-priority: if two overlap, the one added later prevails
         leds_and_sections_new = [self.default_section for led in range(0,self.leds_count-1)]
         for led_num in range(0,self.leds_count-1):
             for section in self.sections_list:
-                print(section.display_name)
+                #print(section.display_name)
                 if (led_num <= section.initial_end_led) and (led_num >= section.initial_start_led):
                     leds_and_sections_new[led_num] = section
-                    print(f"      {section.display_name}")
+                    #print(f"      {section.display_name}")
         #print(leds_and_sections_new)
         for section in self.sections_list:
             if section not in leds_and_sections_new:
@@ -117,9 +121,8 @@ class Strip:
 
 
     def update_light_statuses(self):
-        #for each uniqur section in this strip, call the update_light_status method
-        #after we run this, we should be able to efficienntly assign each led
-        #color/mode to the value of the associated strip's mode value SubSting.get_led_mode()
+        #For each unique section in this strip, call the update_light_status method
+        #After we run this, we should be able to efficienntly assign each pixel status in the Strip
         new_modes_list = [None for led in range(0,self.leds_count-1)]
 
         for section in set(self.sections_list):
@@ -134,8 +137,7 @@ class Strip:
 
 
 
-    def return_sections_and_patterns_dict(self): #this returns a dict of each section's mode (not light status). We can use this for configuring the strip sections.
-
+    def return_sections_and_patterns_dict(self): #Returns a dict of each section's Pattern (not light status). We can use this for configuring the strip sections.
 
         ranges = []
         curr_range_start = 0
@@ -150,12 +152,12 @@ class Strip:
                 curr_range_end = curr_range_start
                 curr_range_pattern = led_section.current_mode
         ranges.append({"range_start": curr_range_start, "range_end": curr_range_end, "range_pattern": curr_range_pattern})
-        #print(ranges)
         return ranges
 
 
-    def return_light_status_as_json(self): #we should really just combine this with the sections thing so we only have to call get_light_status once per led
-        self.update_light_statuses()  # could add something here to not call this if it was called in last X seconds
+    def return_light_status_as_json(self): #Returns an * delimited list of JSON strings representing each Section (and associated light status) in the list.
+        #We chould really just combine this with the sections thing so we only have to call get_light_status once per led
+        self.update_light_statuses()  #Could add something here to not call this if it was called in last X seconds (NO: actually put that stuff in the pattern object, in case pattern is changed)
         ranges = []
         curr_range_start = 0
         curr_range_end = 0
@@ -165,7 +167,6 @@ class Strip:
             if curr_range_status == led_status:
                 curr_range_end += 1
             else:
-                #ranges[range_num] = json.dumps(({"range_start" : curr_range_start,"range_end" : curr_range_end, "range_status" : curr_range_status}))
                 ranges.append( json.dumps(({"range_start" : curr_range_start,"range_end" : curr_range_end, "range_status" : curr_range_status})))
                 curr_range_start = curr_range_end + 1
                 curr_range_end = curr_range_start
@@ -177,12 +178,12 @@ class Strip:
         return "*".join(ranges)
 
 
-class Section:
+class Section: #Class to represent a Section of lights. Has a Pattern.
     def __init__(self, strip ,initial_start_led,initial_end_led, display_name):
         self.section_id = str(strip.strip_id) + str(display_name)
         self.current_leds_count = (initial_end_led - initial_end_led) + 1
         self.current_mode = None
-        self.set_pattern(SolidPattern({'r':255,'g':255,'b':255}, str(uuid.uuid4())))
+        self.set_pattern(SolidPattern({'r':255,'g':255,'b':255}, str(uuid.uuid4()))) #Set the default pattern to a solid 'warm' light color
         self.current_light_status = None
         #self.update_light_status()
         self.initial_end_led = initial_end_led
@@ -193,28 +194,17 @@ class Section:
         self.current_mode = new_mode
 
 
-    def update_light_status(self):
-        #code to automatically update the light status based on current time/weather
+    def update_light_status(self): #Calls the appropriate update function for the associated pattern, and stores the current light status
         #different from setting mode, which sets the pattern (a function relating time/weather and light status)
-        self.current_light_status = self.current_mode.get_current_color_dict()
+        self.current_light_status = self.current_mode.get_current_status_dict()
 
 
 
-    def get_light_status(self):
-        #returns light status.
-
+    def get_light_status(self):#Returns light status.
 
         return self.current_light_status
 
 
-"""
-Here is a template that all of the Pattern classes will follow
-class Pattern:
-    def __int__(self, parameters):
-    
-    def get_current_color_dict:
-    
-"""
 
 
 class Pattern(ABC):
@@ -223,11 +213,14 @@ class Pattern(ABC):
         pass
 
     @abstractmethod
-    def get_current_color_dict():
+    def get_current_status_dict():
         pass
 
     @abstractmethod
     def get_description_string():
+        pass
+
+    def get_description_string_small(self):
         pass
 
 
@@ -244,35 +237,101 @@ class SolidPattern:
         self.pattern_id = pattern_id
 
 
-    def get_current_color_dict(self):
-        return self.color_dict
+    def get_current_status_dict(self): #Returns dict describing the currrent color of the pattern
 
-    def get_description_string(self):
+
+        return {'mode':'so','color':self.color_dict}
+
+    def get_description_string(self): #Gives string describing pattern type and current status
         return f"Solid color: {self.color_dict}"
 
-    def get_description_string_small(self):
+    def get_description_string_small(self): #Gives string descring only pattern type
         return f"Solid color pattern"
 
 
 class TimePattern:
-    def __init__(self, time_color_dicts, gradient_type, pattern_id):
-        #Format of time_color_dicts: (0:{'r':255,'g':255,'b':255},180:{'r':255,'g':255,'b':255},600:{'r':255,'g':255,'b':255}), where the numbers 0, 180, and 600 represent minutes since the start of the day (week if we make that an option)
+    def __init__(self, time_color_dicts, gradient_type, time_zone, pattern_id):
+        #Format of time_color_dicts: (0:{'r':255,'g':255,'b':255},180:{'r':255,'g':255,'b':255},600:{'r':255,'g':255,'b':255}), where the numbers 0, 180, and 600 represent minutes since the start of the day
         #Format of gradient_type
-        #time zone offset is nummber of hours to add to UTC to get the time zone (DST?)
+
         self.time_color_table = pd.DataFrame.from_dict(time_color_dicts)
         self.gradient_type = gradient_type
-        self.time_zone_offset = time_zone_offset
+        self.time_zone = time_zone
         self.pattern_id = pattern_id
+        self.color_dict = None
 
-    def get_current_color_dict(self):
-        period_minutes #= minutes since day begin
+    def get_current_status_dict(self):
+        now = datetime.datetime.now(pytz.timezone(self.time_zone))
+
+        minutes_since_midnight = round((now - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds()/60)
         if gradient_type == "fade":
-            color_dict = self.time_color_table.append(pd.Series({'r':np.NaN,'g':np.NaN,'b':np.NaN},name=period_minutes)).interpolate(method='index').loc[period_minutes].to_dict.round().to_dict()
+            self.color_dict = self.time_color_table.append(pd.Series({'r':np.NaN,'g':np.NaN,'b':np.NaN},name=minutes_since_midnight)).interpolate(method='index').loc[minutes_since_midnight].to_dict.round().to_dict()
 
+        return {'mode':'solid','color':self.color_dict}
 
     def get_description_string(self):
-        return f"Time-based pattern, current color: {self.get_current_color_dict()}"
+        return f"Time-based pattern, current color: {self.color_dict}"
 
     def get_description_string_small(self):
         return f"Time-based pattern"
+
+
+class DynamicPattern:
+    def __init__(self, pattern_name, pattern_id):
+        self.pattern_id = pattern_id;
+        self.pattern_name = pattern_name;
+        self.pattern_code = self.DYNAMIC_PATTERN_CODES[pattern_name]
+
+    def get_current_status_dict(self):
+        return {'mode':self.pattern_code, 'color':{'r':0,'g':0,'b':0}}
+
+    def get_description_string(self):
+        return f"{self.pattern_name} pattern"
+
+    def get_description_string_small(self):
+        return f"{self.pattern_name} pattern"
+
+
+class SunPattern:
+    def __init__(self, lat, long, time_zone, sun_down_color_dict, sun_up_color_dict, pattern_id):
+        self.pattern_id = pattern_id
+        self.sun_down_color_dict = sun_down_color_dict
+        self.sun_up_color_dict = sun_up_color_dict
+        self.lat = lat
+        self.long = long
+        self.time_zone = time_zone
+
+    def get_current_status_dict(self):
+        now = datetime.datetime.now(pytz.timezone(self.time_zone))
+
+        minutes_since_midnight_now = round((now - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds()/60)
+
+        loc = astral.Location(('place','place',lat,lon,self.time_zone))
+
+        minutes_since_midnight_dawn = round((loc.dawn() - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds()/60)
+
+        minutes_since_midnight_sunrise = round((loc.sunrise() - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds() / 60)
+        minutes_since_midnight_sunset = round(
+            (loc.sunset() - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds() / 60)
+        minutes_since_midnight_dusk= round(
+            (loc.dusk() - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds() / 60)
+
+
+        self.time_color_table =  pd.DataFrame.from_dict({0:self.sun_down_color_dict, minutes_since_midnight_dawn:self.sun_down_color_dict,minutes_since_midnight_sunrise:self.sun_up_color_dict,minutes_since_midnight_sunset:self.sun_up_color_dict,minutes_since_midnight_dusk:self.sun_down_color_dict})
+
+
+        current_color = self.time_color_table.append(pd.Series({'r':np.NaN,'g':np.NaN,'b':np.NaN},name=minutes_since_midnight)).interpolate(method='index').loc[minutes_since_midnight].to_dict.round().to_dict()
+        return {'mode':'solid','color':current_color}
+
+
+    def get_description_string(self):
+        return "Sun-based pattern"
+
+    def get_description_string_small(self):
+        return "Sun-based pattern"
+
+
+
+
+
 
--- app.py
+++ app.py
@@ -5,18 +5,19 @@ from okta import UsersClient
 import pickle
 import uuid
 
+#Initialize the components needed for the Okta accounts system
 
 
-try:
+try: #Try to load the database from the saved pickle, make a new one if that fails
     main_database = pickle.load(open("user_database.p",'rb'))
 except:
     main_database = MainDatabase()
 
-def save_database():
+def save_database(): #Save the database to a pickle file
     pickle.dump(main_database,open("user_database.p",'wb'))
 
 @app.before_request
-def before_request():
+def before_request(): #Code for Okta login
     if oidc.user_loggedin:
 
         g.user = okta_client.get_user(oidc.user_getfield("sub"))
@@ -25,13 +26,13 @@ def before_request():
 
 
 @app.route("/")
-def index():
+def index(): #Serve homepage
     return render_template("index.html")
 
 
 @app.route("/dashboard")
 @oidc.require_login
-def dashboard():
+def dashboard(): #Serve dashboard page
     user_object = main_database.return_user_object(g.user)
     save_database()
     return render_template("dashboard.html", user_object=user_object)
@@ -39,18 +40,18 @@ def dashboard():
 
 @app.route("/login")
 @oidc.require_login
-def login():
+def login(): #Serve login page redirect
     return redirect(url_for(".dashboard"))
 
 
 @app.route("/logout")
-def logout():
+def logout(): #Process logout, redirect to homepage
     oidc.logout()
     return redirect(url_for(".index"))
 
 @app.route("/add_strip/<strip_id>/<leds_count>/<display_name>")
 @oidc.require_login
-def add_strip(strip_id, leds_count, display_name):
+def add_strip(strip_id, leds_count, display_name): #Process adding a strip
 
     strip_id = strip_id.lower().strip(":").strip(" ")
     leds_count = int(leds_count)
@@ -65,7 +66,7 @@ def add_strip(strip_id, leds_count, display_name):
 
 
 @app.route("/get_strip_status/<strip_id>/")
-def get_strip_status(strip_id):
+def get_strip_status(strip_id): #Serve the strip status
     #print(main_database.strips_database)
     #strip_obj = main_database.strips_database.loc[strip_id]["Strip Object"]
     try:
@@ -77,7 +78,7 @@ def get_strip_status(strip_id):
 
 
 @app.route("/add_section/<strip_id>/<start_led>/<end_led>")
-def add_section(strip_id,start_led,end_led):
+def add_section(strip_id,start_led,end_led): #Process adding a section
     #print(main_database.strips_database)
     #strip_obj = main_database.strips_database.loc[strip_id]["Strip Object"]
     #strip_obj.add_new_section_and_update(int(start_led), int(end_led))
@@ -97,9 +98,9 @@ def add_section(strip_id,start_led,end_led):
         return f"{e}:Strip not found or other error"
 
 
-@app.route("/set_section_solid_pattern/<strip_id>/<section_id>/<R>,<G>,<B>") #JUST FOR TESting, need to accommodate different info block parameters
+@app.route("/set_section_solid_pattern/<strip_id>/<section_id>/<R>,<G>,<B>") #Only for SolidPatterns at the moment
 @oidc.require_login
-def set_section_solid_pattern(strip_id, section_id,R,G,B):
+def set_section_solid_pattern(strip_id, section_id,R,G,B): #Process setting a section pattern
     #print(main_database.strips_database)
     strip_obj = main_database.strips_database.loc[strip_id]["Strip Object"]
     section_obj = strip_obj.get_section_by_id(section_id)
@@ -113,7 +114,7 @@ def set_section_solid_pattern(strip_id, section_id,R,G,B):
 
 @app.route("/set_pattern_solid_color/<strip_id>/<pattern_id>/<R>,<G>,<B>") #JUST FOR TESting, need to accommodate different info block parameters
 @oidc.require_login
-def set_pattern_solid_color(strip_id, pattern_id,R,G,B):
+def set_pattern_solid_color(strip_id, pattern_id,R,G,B): #Process moodifying a SolidPattern's color
     #print(main_database.strips_database)
     strip_obj = main_database.strips_database.loc[strip_id]["Strip Object"]
     pattern_obj = strip_obj.get_pattern_by_id(pattern_id)
@@ -123,5 +124,13 @@ def set_pattern_solid_color(strip_id, pattern_id,R,G,B):
 
     else:
         return "Error finding that section"
+
+@app.route("/create_sunrise_sunet_pattern/<strip_id>/<section_id>") #JUST FOR TESting, need to accommodate different info block parameters
+@oidc.require_login
+def create_sunrise_sunset_page(strip_id, section_id):
+    lat = reader.get(request.remote_addr)['location']['latitude']
+    lon = reader.get(request.remote_addr)['location']['longitude']
+
+    return "something"
 
 
